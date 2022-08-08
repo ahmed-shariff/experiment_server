41b240a84fb4deb25b6ea7740981e828b68d2424
Ahmed Shariff <shariff.mfa@outlook.com>
2022-08-07 23:58:21 -0700

    Fix issue with cli test

--- test/test_cli.py
+++ test/test_cli.py
@@ -1,7 +1,10 @@
 import pytest
+import importlib
 from click.testing import CliRunner
 import experiment_server
 import pytest_mock
+
+import experiment_server.cli
 
 
 @pytest.fixture(scope="function")
@@ -9,20 +12,19 @@ def runner():
     return CliRunner()
 
 
-@pytest.fixture(scope="function")
-def cli(mocker):
-    mocker.patch("experiment_server._main._main")
-    mocker.patch("experiment_server._process_config.verify_config")
+def mock_function(mocker, mock_function):
+    mocker.patch(mock_function)
     # Before the _main method gets imported need to mock them
-    from experiment_server.cli import cli
-    return None
+    importlib.reload(experiment_server.cli)
 
 
-def test_run(cli, runner):
-    result = runner.invoke(cli, ["run", "file"])
+def test_run(runner, mocker):
+    mock_function(mocker, "experiment_server._main._main")
+    result = runner.invoke(experiment_server.cli.cli, ["run", "file"])
     experiment_server._main._main.assert_called_with(None, '127.0.0.1', '5000', "file")
 
 
-def test_verify_config(cli, runner):
-    result = runner.invoke(cli, ["verify-config-file", "file"])
+def test_verify_config(runner, mocker):
+    mock_function(mocker, "experiment_server._process_config.verify_config")
+    result = runner.invoke(experiment_server.cli.cli, ["verify-config-file", "file"])
     experiment_server._process_config.verify_config.assert_called_with("file")
