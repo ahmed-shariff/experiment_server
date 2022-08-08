2f526609706c97f962adb4d13404d7a3614854f6
Ahmed Shariff <shariff.mfa@outlook.com>
2022-08-08 00:00:51 -0700

    commnting out _construct_participant_condition_old

--- experiment_server/_participant_ordering.py
+++ experiment_server/_participant_ordering.py
@@ -83,43 +83,43 @@ def construct_participant_condition(config: List[Dict], participant_index: int, 
         return [config[step_names[i]] for i in chained_order]
     
 
-def _construct_participant_condition_old(config, participant_index, use_latin_square=False, latin_square=None, config_categorization=None, default_configuration=None, randomize=True):
-    if participant_index < 1:
-        participant_index = 1
-    if use_latin_square:
-        _config = [config[i - 1] for i in latin_square[(participant_index - 1) % len(config)]]
-    else:
-        assert len(config_categorization) == 2
-        if not randomize or participant_index % len(config_categorization) == 0:
-            init_condition = config_categorization[0][:]
-            other_condition = config_categorization[1][:]
-        else:
-            init_condition = config_categorization[1][:]
-            other_condition = config_categorization[0][:]
-        init_condition = [config[i] for i in init_condition]
-        other_condition = [config[i] for i in other_condition]
-        random.shuffle(init_condition)
-        random.shuffle(other_condition)
+# def _construct_participant_condition_old(config, participant_index, use_latin_square=False, latin_square=None, config_categorization=None, default_configuration=None, randomize=True):
+#     if participant_index < 1:
+#         participant_index = 1
+#     if use_latin_square:
+#         _config = [config[i - 1] for i in latin_square[(participant_index - 1) % len(config)]]
+#     else:
+#         assert len(config_categorization) == 2
+#         if not randomize or participant_index % len(config_categorization) == 0:
+#             init_condition = config_categorization[0][:]
+#             other_condition = config_categorization[1][:]
+#         else:
+#             init_condition = config_categorization[1][:]
+#             other_condition = config_categorization[0][:]
+#         init_condition = [config[i] for i in init_condition]
+#         other_condition = [config[i] for i in other_condition]
+#         random.shuffle(init_condition)
+#         random.shuffle(other_condition)
 
-        if default_configuration is not None:
-            default_configuration_config = default_configuration[0]["config"]
-            non_default_keys = [k for k in default_configuration_config.keys() if k not in ["conditionId"]]
+#         if default_configuration is not None:
+#             default_configuration_config = default_configuration[0]["config"]
+#             non_default_keys = [k for k in default_configuration_config.keys() if k not in ["conditionId"]]
 
-            init_condition_train = init_condition[0].copy()
-            init_condition_train["config"] = init_condition_train["config"].copy()
-            init_condition_train["config"]["conditionId"] = "training1"
-            for k in non_default_keys:
-                init_condition_train["config"][k] = default_configuration_config[k]
+#             init_condition_train = init_condition[0].copy()
+#             init_condition_train["config"] = init_condition_train["config"].copy()
+#             init_condition_train["config"]["conditionId"] = "training1"
+#             for k in non_default_keys:
+#                 init_condition_train["config"][k] = default_configuration_config[k]
 
-            other_condition_train = other_condition[0].copy()
-            other_condition_train["config"] = other_condition_train["config"].copy()
-            other_condition_train["config"]["conditionId"] = "training2"
-            for k in non_default_keys:
-                other_condition_train["config"][k] = default_configuration_config[k]
+#             other_condition_train = other_condition[0].copy()
+#             other_condition_train["config"] = other_condition_train["config"].copy()
+#             other_condition_train["config"]["conditionId"] = "training2"
+#             for k in non_default_keys:
+#                 other_condition_train["config"][k] = default_configuration_config[k]
 
-            _config = [init_condition_train] + init_condition + [other_condition_train] + other_condition
+#             _config = [init_condition_train] + init_condition + [other_condition_train] + other_condition
 
-        else:
-            _config = init_condition + other_condition
-        # _config = [config[i] for i in _config_list]
-    return _config
+#         else:
+#             _config = init_condition + other_condition
+#         # _config = [config[i] for i in _config_list]
+#     return _config
