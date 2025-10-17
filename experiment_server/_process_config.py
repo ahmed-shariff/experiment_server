186e2d909acd548171275e451ca4601c978bb722
Shariff Faleel <shariff.mfa@outlook.com>
2025-10-17 10:02:40 -0700

    Remove expconfig related code

--- experiment_server/_process_config.py
+++ experiment_server/_process_config.py
@@ -1,14 +1,11 @@
 from pathlib import Path
 import random
 import warnings
-from shutil import ExecError
 from typing import Any, Callable, Dict, List, Tuple, Union
 
-from tornado.locale import load_translations
 from experiment_server._participant_ordering import construct_participant_condition, ORDERING_STRATEGY
 from experiment_server.utils import ExperimentServerConfigurationExcetion, ExperimentServerExcetion, merge_dicts
 from loguru import logger
-from easydict import EasyDict as edict
 import json
 import toml
 
@@ -18,44 +15,14 @@ SECTIONS = ["main_configuration", "init_configuration", "final_configuration", "
 ALLOWED_SETTINGS = ["randomize_within_groups", "randomize_groups"]
 
 
-def get_sections(f: Union[str, Path]) -> Dict[str, str]:
-    loaded_configurations = {}
-    current_blob = None
-    current_section = None
-    with open(f) as fp:
-        for line in fp.readlines():
-            stripped_line = line.strip()
-            if stripped_line.startswith("//"):
-                stripped_line = stripped_line.lstrip("//")
-                if stripped_line in SECTIONS:   # Lines starting with // are considered comments
-                    if current_blob is not None:
-                        loaded_configurations[current_section] = current_blob
-                    current_blob = ""
-                    current_section = stripped_line
-            else:
-                try:
-                    line = line.rstrip()
-                    if len(line) > 0:
-                        current_blob += line
-                except TypeError:
-                    raise ExperimentServerConfigurationExcetion("The file should start with a section header.")
-                    
-        loaded_configurations[current_section] = current_blob
-
-    logger.info(f"Sections in configuration: {list(loaded_configurations.keys())}")
-    return loaded_configurations
-
-
 def process_config_file(f: Union[str, Path], participant_index: int, supress_message:bool=False) -> List[Dict[str, Any]]:
     if participant_index < 1:
         raise ExperimentServerConfigurationExcetion(f"Participant index needs to be greater than 0, got {participant_index}")
 
-    if Path(f).suffix == ".expconfig":
-        return _process_expconfig(f, participant_index, supress_message)
-    elif Path(f).suffix == ".toml":
+    if Path(f).suffix == ".toml":
         return _process_toml(f, participant_index, supress_message)
     else:
-        raise ExperimentServerExcetion("Invalid file type. Expected `.expconfig` or `.toml`")
+        raise ExperimentServerExcetion("Invalid file type. Expected `.toml`")
 
 
 def _process_toml(f: Union[str, Path], participant_index:int, supress_message:bool=False) -> List[Dict[str, Any]]:
@@ -127,74 +94,6 @@ def _process_toml(f: Union[str, Path], participant_index:int, supress_message:bo
     if not supress_message:
         logger.info("Configuration loaded: \n" + json.dumps(blocks, indent=2))
     return blocks
-
-
-def _process_expconfig(f: Union[str, Path], participant_index: int, supress_message:bool=False) -> List[Dict[str, Any]]:
-    loaded_configurations = get_sections(f)
-    if "template_values" in loaded_configurations:
-        template_values = json.loads(loaded_configurations["template_values"])
-    else:
-        template_values = {}
-    
-    # config = json.loads(_replace_template_values(loaded_configurations["init_configuration"], template_values))
-    if "order" in loaded_configurations:
-        order = json.loads(loaded_configurations["order"])
-    else:
-        order = [list(range(len(loaded_configurations)))]
-
-    if "settings" in loaded_configurations:
-        settings = edict(json.loads(loaded_configurations["settings"]))
-    else:
-        settings = edict()
-
-    settings.groups = settings.get("groups", ORDERING_STRATEGY.as_is)
-    settings.within_groups = settings.get("within_groups", ORDERING_STRATEGY.as_is)
-
-    logger.info(f"Settings used: \n {json.dumps(settings, indent=4)}")
-
-    _raw_main_configuration = loaded_configurations["main_configuration"]
-    _templated_main_configuration = _replace_template_values(_raw_main_configuration, template_values)
-    try:
-        main_configuration = json.loads(_templated_main_configuration)
-    except Exception as e:
-        logger.error("Raw main config: " + _raw_main_configuration)
-        logger.error("Main config with template values passed: " + _templated_main_configuration)
-        if isinstance(e, json.decoder.JSONDecodeError):
-            raise ExperimentServerConfigurationExcetion("JSONDecodeError at position {}: `... {} ...`".format(
-                e.pos,
-                _templated_main_configuration[max(0, e.pos - 40):min(len(_templated_main_configuration), e.pos + 40)]))
-        else:
-            raise
-    main_configuration = construct_participant_condition(main_configuration, participant_index, order=order,
-                                                         init_block_names=[],
-                                                         final_block_names=[],
-                                                         groups_strategy=settings.groups,
-                                                         within_groups_strategy=settings.within_groups)
-
-    if "init_configuration" in loaded_configurations:
-        init_configuration = json.loads(_replace_template_values(loaded_configurations["init_configuration"], template_values))
-    else:
-        init_configuration = []
-        
-    if "final_configuration" in loaded_configurations:
-        final_configuration = json.loads(_replace_template_values(loaded_configurations["final_configuration"], template_values))
-    else:
-        final_configuration = []
-
-    config = init_configuration + main_configuration + final_configuration
-    config = resolve_extends(config)
-
-    try:
-        for (idx, c) in enumerate(config):
-            c["config"]["participant_index"] = participant_index
-            c["config"]["name"] = c["name"]
-            c["config"]["block_id"] = idx
-    except KeyError:
-        raise ExperimentServerConfigurationExcetion("blocks missing keys (config/name)")
-    
-    if not supress_message:
-        logger.info("Configuration loaded: \n" + json.dumps(config, indent=2))
-    return config
 
 
 def _resolve_extends(c, configs, seen_configs):
--- test/test_process_config.py
+++ test/test_process_config.py
@@ -4,7 +4,7 @@ import pytest_mock
 from deepdiff import DeepDiff
 import random
 
-from experiment_server._process_config import verify_config, get_sections, process_config_file, _process_expconfig, _process_toml, resolve_extends, ChoicesFunction, _resolve_function
+from experiment_server._process_config import verify_config, _process_toml, resolve_extends, ChoicesFunction, _resolve_function
 from experiment_server.utils import ExperimentServerConfigurationExcetion
 
 
@@ -18,14 +18,7 @@ MAIN_CONFIG_KEYS = ["buttonSize","trialsPerItem","conditionId","relativePosition
         (Path(__file__).parent / "test_files/working_file_7.toml", True),
         (Path(__file__).parent / "test_files/working_file_8.toml", True),
         (Path(__file__).parent / "test_files/working_file_9.toml", True),
-        (Path(__file__).parent / "test_files/working_file.expconfig", True),
-        (Path(__file__).parent / "test_files/working_file_2.expconfig", True),
-        (Path(__file__).parent / "test_files/working_file_3.expconfig", True),
-        (Path(__file__).parent / "test_files/working_file_4.expconfig", True),
-        (Path(__file__).parent / "test_files/working_file_5.expconfig", True),
-        (Path(__file__).parent / "test_files/working_file_6.expconfig", True),
-        (Path(__file__).parent / "test_files/failing_config.expconfig", False),
-        (Path(__file__).parent / "test_files/failing_config_2.expconfig", False)])
+        (Path(__file__).parent / "test_files/failing_config.toml", False)])
 def test_verify_config(f, expected):
     out = verify_config(f)
     assert out == expected
@@ -34,10 +27,7 @@ def test_verify_config(f, expected):
 def _test_func(config):
     other_config_keys = ["conditionId", "participant_index", "name", "block_id"]
     for c in config:
-        if c["name"] in ["rating", "configuration"]:
-            config_keys = other_config_keys
-        else:
-            config_keys = MAIN_CONFIG_KEYS
+        config_keys = MAIN_CONFIG_KEYS
         keys = list(c["config"].keys())
         if len(c["config"]) == len(config_keys):
             if not all([k in keys for k in config_keys]):
@@ -50,54 +40,10 @@ def _test_func(config):
 @pytest.mark.parametrize(
     "f, expected",[
         (Path(__file__).parent / "test_files/working_file.toml", True),
-        (Path(__file__).parent / "test_files/working_file.expconfig", True),
-        (Path(__file__).parent / "test_files/working_file_2.expconfig", False),
-        (Path(__file__).parent / "test_files/working_file_3.expconfig", False),
-        (Path(__file__).parent / "test_files/working_file_4.expconfig", True),
-        (Path(__file__).parent / "test_files/failing_config.expconfig", False)])
+        ])
 def test_verify_config_with_test_func(f, expected):
     out = verify_config(f, test_func=_test_func)
     assert out == expected
-
-
-@pytest.mark.parametrize(
-    "f, expected",[
-        (Path(__file__).parent / "test_files/working_file.expconfig", ['main_configuration', 'init_configuration', 'final_configuration', 'template_values', 'order']),
-        (Path(__file__).parent / "test_files/working_file_6.expconfig", ['main_configuration', 'order']),
-        (Path(__file__).parent / "test_files/working_file_4.expconfig", ['main_configuration', 'init_configuration', 'final_configuration', 'template_values', 'order']),
-        ])
-def test_get_sections(f, expected):
-    loaded_configs = get_sections(f)
-    loaded_sections = list(loaded_configs.keys())
-    assert len(set(loaded_sections).difference(expected)) == 0
-
-
-@pytest.mark.parametrize(
-    "f, pid", [
-        (Path(__file__).parent / "test_files/working_file.expconfig", 1),
-        (Path(__file__).parent / "test_files/working_file.expconfig", 2),
-        (Path(__file__).parent / "test_files/working_file_4.expconfig", 1),
-        (Path(__file__).parent / "test_files/working_file_4.expconfig", 2),
-        ])
-def test_process_expconfig(mocker, f, pid):
-    import experiment_server._process_config
-    spy_get_sections = mocker.spy(experiment_server._process_config, "get_sections")
-    config = _process_expconfig(f, pid)
-    spy_get_sections.assert_called_once_with(f)
-    assert len(config) == 10
-    assert config[0]["name"] == "configuration"
-    assert config[-1]["name"] == "rating"
-
-    for c in config:
-        assert "config" in c
-        assert "name" in c
-        assert "name" in c["config"]
-        assert "participant_index" in c["config"]
-        assert c["config"]["participant_index"] == pid
-
-    for block_id in range(1, 10):
-        keys = set(config[block_id]["config"].keys())
-        assert len(keys.difference(MAIN_CONFIG_KEYS)) == 0
 
 
 @pytest.mark.parametrize(
